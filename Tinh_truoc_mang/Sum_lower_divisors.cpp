a1999361156b762f3ab81d3821b3eafaaea8fe56
Nguyen Hoang Phuc <59410730+nhphucqt@users.noreply.github.com>
2020-01-08 22:34:07 +0700

    Update Sum_lower_divisors.cpp

--- Tinh_truoc_mang/Sum_lower_divisors.cpp
+++ Tinh_truoc_mang/Sum_lower_divisors.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+const int nmax = 1000007;
+int n, sum[2*nmax];
+
+void sum_divisor()
+{
+    fill(sum+2, sum+nmax+1, 1);
+    for (int i = 2; i*i <= nmax; ++i)
+    for (int j = i; j <= nmax/i; ++j)
+    {
+        sum[i*j] += i + j*(i!=j);
+    }
+}
+
+int main()
+{
+    cin >> n;
+    sum_divisor();
+    cout << sum[n] << '\n';
+    return 0;
+}
