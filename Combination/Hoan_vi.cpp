ead4b1e455da563b53a71c1b03fb192e10f5d519
Nguyen Hoang Phuc <59410730+nhphucqt@users.noreply.github.com>
2020-01-08 21:16:46 +0700

    Create Hoan_vi.cpp

---
+++ Combination/Hoan_vi.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int nmax = 11;
+int n, ch[nmax];
+bool used[nmax];
+ 
+void print()
+{
+    for(int i = 1; i <= n; ++i) cout << ch[i] << ' ';
+    cout << '\n';
+}
+ 
+void hv(const int& pos)
+{
+    for(int i = 1; i <= n; ++i)
+    {
+        if (!used[i])
+        {
+            ch[pos] = i;
+            used[i] = true;
+            if (pos == n) print();
+            else hv(pos+1);
+            used[i] = false;
+        }
+    }
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n;
+    hv(1);
+    return 0;
+}
