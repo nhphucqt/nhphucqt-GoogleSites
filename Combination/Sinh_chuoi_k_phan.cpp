2ff7557c9e0d5ceeb081c609377ca7f12e291e4d
Nguyen Hoang Phuc <59410730+nhphucqt@users.noreply.github.com>
2020-01-08 21:15:01 +0700

    Create Sinh_chuoi_k_phan.cpp

---
+++ Combination/Sinh_chuoi_k_phan.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int nmax = 100;
+int n, k, ch[nmax];
+ 
+void print()
+{
+    for(int i = 1; i <= n; ++i) cout << ch[i] << ' ';
+    cout << '\n';
+}
+ 
+void chon(const int& pos)
+{
+    for (int i = 0; i <= k-1; ++i)
+    {
+        ch[pos] = i;
+        if (pos == n) print();
+        else chon(pos+1);
+    }
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> k >> n;
+    chon(1);
+    return 0;
+}
