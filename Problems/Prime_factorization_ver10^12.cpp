77d776232d1a99ff5abea6b0f00c63791d2e0c56
Nguyen Hoang Phuc <59410730+nhphucqt@users.noreply.github.com>
2020-04-01 20:04:40 +0700

    Update Prime_factorization_ver10^12.cpp

--- Problems/Prime_factorization_ver10^12.cpp
+++ Problems/Prime_factorization_ver10^12.cpp
@@ -7,25 +7,20 @@ using namespace std;
 typedef long long ll;
 ll n;
 
-ll min_d(ll n)
+ll min_d(ll n, ll p)
 {
-    if (n < 2) return 1;
-    if (n % 2 == 0) return 2;
-    if (n % 3 == 0) return 3;
-    for (long long i = 5; i*i <= n; i+=6)
-    {
+    for (ll i = p; i*i <= n; ++i)
         if (n % i == 0) return i;
-        if (n % (i+2) == 0) return i+2;
-    }
     return n;
 }
 
 vector<pair<ll,int>> factorize(ll n)
 {
     vector<pair<ll, int>> f;
+    ll p = 2;
     while (n > 1)
     {
-        long long p = min_d(n);
+        p = min_d(n, p);
         for (f.push_back({p, 0}); n % p == 0; ++f.back().Y) n /= p;
     }
     return f;
