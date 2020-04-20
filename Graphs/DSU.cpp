048443e58610dab07566e9e77f5175a21b8b4a93
Nguyen Hoang Phuc <59410730+nhphucqt@users.noreply.github.com>
2020-04-20 14:08:10 +0700

    Update DSU.cpp

--- Graphs/DSU.cpp
+++ Graphs/DSU.cpp
@@ -21,9 +21,6 @@ void Unionroot(int u, int v) // O(1) -- u, v are roots
 int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    freopen("A.INP", "r", stdin);
-    freopen("A.OUT", "w", stdout);
-
     memset(Fa, -1, sizeof Fa);
     cin >> n >> m;
     for (int i = 1; i <= m; ++i)
