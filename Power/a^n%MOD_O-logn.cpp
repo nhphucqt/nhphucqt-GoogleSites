7c5a64c370ec7459a0284eee2d4ffef2557c2548
Nguyen Hoang Phuc <59410730+nhphucqt@users.noreply.github.com>
2020-06-03 23:02:04 +0700

    Update a^n%MOD_O-logn.cpp

--- Power/a^n%MOD_O-logn.cpp
+++ Power/a^n%MOD_O-logn.cpp
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
+
 long long a, n;
 int MOD;
- 
-int Pow(long long a, long long n, int MOD)
+
+int Pow(int a, long long n, int MOD)
 {
     if (n == 0) return 1;
     if (n == 1) return a;
-    long long tmp = Pow(a%MOD, n/2, MOD);
-    if (n % 2 == 0) return tmp*tmp % MOD;
-    else return (tmp*tmp)%MOD * a % MOD;
+    long long tmp = Pow(a, n/2, MOD);
+    tmp = tmp * tmp % MOD;
+    if (n&1) tmp = tmp * a % MOD;
+    return tmp;
 }
- 
+
 int main()
 {
     cin >> a >> n >> MOD;
